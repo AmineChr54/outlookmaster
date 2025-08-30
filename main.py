30ac15544eebcea1650cd0f7d3ba4dc58572a599
Amine Cheikhrouhou <amine.cheikhrouhou54@gmail.com>
2025-08-30 16:01:27 +0200

    Basis

---
+++ main.py
@@ -0,0 +1,9 @@
+# Use a pipeline as a high-level helper
+from transformers import pipeline
+
+pipe = pipeline("text-generation", model="openai-community/gpt2")
+prompt = "Hello"
+
+output = pipe(prompt, max_length=200, do_sample=True, temperature=0.7)
+print(output[0]['generated_text'])
+
